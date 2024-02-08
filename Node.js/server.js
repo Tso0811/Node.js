6bf517c325fed791b5c50d032948bae47a854f26
Tso0811 <savagetso0811@gmail.com>
2024-02-08 12:28:24 +0800

    Include module

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
+const dateTime = require("./myFirstModule");
 http.createServer(function (req,res){
     req.statusCode = 200 ;
     res.setHeader("Content-Type","text/html ; charset=utf-8");
-    res.end("你好");
-
-}).listen(8000);
+    res.end(dateTime.dateTime());
+}).listen(8000);    
